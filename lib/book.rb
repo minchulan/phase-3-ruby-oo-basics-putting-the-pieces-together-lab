791240dfdb6f294322eec87b6f7e8450ee7c0a56
Minchul An <min@minchulan.io>
2023-01-25 02:35:00 -0500

    commit tree 942ef6b5309823dfd3486a33688e1c9fed14913b

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,47 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre 
+    def initialize(title)
+        @title = title 
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end 
+end 
+book = Book.new("And Then There Were None")
+
+
+
+
+
+# attr_accessor :author is equivalent to:
+#     def author=(author)
+#         @author = author 
+#     end 
+
+#     def author
+#         @author 
+#     end 
+
+# , "Agatha Christie", 272, "Mystery"
+
+# class Book
+#     attr_accessor :title, :author, :page_count, :genre 
+# end 
+
+# book = Book.new 
+
+# book.title = "And Then There Were None"
+# book.author = "Agatha Christie"
+# book.page_count = 272
+# book.genre = "Mystery"
+
+
+
+# Book #turn_page can turn the page:
+#   describe '#turn_page' do
+#     it 'can turn the page' do
+#       book = Book.new("The World According to Garp")
+#       expect { book.turn_page }.to output(a_string_matching("Flipping the page...wow, you read fast!")).to_stdout
+#     end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,35 @@
-# Make your shoe class here!
+
+class Shoe 
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand = "Nike")
+        @brand = brand 
+    end 
+
+    def cobble
+        puts "Your shoe is as good as new!" 
+    end 
+end 
+
+shoe = Shoe.new
+shoe.cobble = "new"
+puts shoe.cobble 
+
+    # def condition=(condition)
+    #     @condition = condition
+    # end 
+
+
+
+# describe '#cobble' do
+#     let(:shoe) { Shoe.new("Nike") }
+    
+#     it 'says that the shoe has been repaired' do
+#       expect { shoe.cobble }.to output(a_string_matching("Your shoe is as good as new!")).to_stdout
+#     end
+    
+#     it 'makes the shoe\'s condition new' do
+#       shoe.condition = "old"
+#       shoe.cobble
+#       expect(shoe.condition).to eq("new")
+#     end
